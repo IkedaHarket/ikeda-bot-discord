5fcc910fc4763ed64f3fcc2188dbd0f5dc50e5cf
Ikeda Harket <sebaaignacio111@gmail.com>
2021-12-26 12:16:54 -0300

    26-12-21

--- coingecko/tokens.js
+++ coingecko/tokens.js
@@ -5,39 +5,44 @@ const coinGeckoApi = 'https://api.coingecko.com/api/v3/simple/'
 const {SLP,MASTER,BTC, SOL, LUNA} = require('./dataCoins')
 
 
+const getTokenPrice = async(token)=> await axios.get(`${coinGeckoApi}token_price/${token.platform}?contract_addresses=${token.contract}&vs_currencies=usd`)
+
+const getPrice = async(cripto)=> await axios.get(`${coinGeckoApi}price?ids=${cripto}&vs_currencies=usd`)
 
 const getPriceToken = async(token) =>{
     let data = {};
+    
     switch (token) {
 
         case 'SLP':
-            data = await axios.get(`${coinGeckoApi}token_price/${SLP.platform}?contract_addresses=${SLP.contract}&vs_currencies=usd`)
+            data = await getTokenPrice(SLP)
             break;
-
+            
         case 'MASTER':
-            data = await axios.get(`${coinGeckoApi}token_price/${MASTER.platform}?contract_addresses=${MASTER.contract}&vs_currencies=usd`)
-            break;
-
+            data = await getTokenPrice(MASTER)
+        break;
+                
         case 'BTC':
         case 'BITCOIN':
-            data = await axios.get(`${coinGeckoApi}price?ids=${BTC}&vs_currencies=usd`)
+            data = await getPrice(BTC)
         break;
 
         case 'SOL':
         case 'SOLANA':
-            data = await axios.get(`${coinGeckoApi}price?ids=${SOL}&vs_currencies=usd`)
+            data = await getPrice(SOL)
         break;
 
         case 'TERRA':
         case 'LUNA':
-            data = await axios.get(`${coinGeckoApi}price?ids=${LUNA}&vs_currencies=usd`)
+            data = await getPrice(LUNA)
         break;
-
+        
         default:
             return 'Moneda no agregada';
     }
     return Object.values(data.data)[0].usd
 }
+
 
 module.exports = {
     getPriceToken
